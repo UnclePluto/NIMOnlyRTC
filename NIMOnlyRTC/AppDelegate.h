9b2df44448a548c2770f08cf8a8e3bd332d322cd
UnclePluto <dypluto@163.com>
2017-06-28 13:14:02 +0800

    Initial Commit

---
+++ NIMOnlyRTC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NIMOnlyRTC
+//
+//  Created by Nick Deng on 2017/6/28.
+//  Copyright © 2017年 Nick Deng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
